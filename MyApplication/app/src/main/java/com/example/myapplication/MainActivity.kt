acdc0b5021a50a191fed45eba61f6bb3cac23a81
unknown <ghappy816@gmail.com>
2021-08-08 22:01:54 +0530

    Inial commit

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    val TAG = "MyApp"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate: ")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: ")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart: ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: ")
+    }
+
+    fun showToast(view: View) {
+        Toast.makeText(this, "this button is clicked", Toast.LENGTH_SHORT).show()
+    }
+
+}
